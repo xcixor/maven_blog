de494a1eeced0d722e5335ba7c9f6a80a42e167d
xcixor <pndungu54@gmail.com>
2022-12-22 13:53:20 +0300

    chore(debugging): setup additional debugging tools to improve dev process

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import chalk from 'chalk';
+import debug from 'debug';
+import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import usersRoutes from './routes/users.js';
 
+
 const app = express();
 const PORT = 5000;
+
+app.use(morgan('tiny'));
 
 app.use(bodyParser.json());
 
@@ -13,4 +19,4 @@ app.get('/', (req, res)=> {
     res.send('Hello from xcixor')
 });
 
-app.listen(PORT, ()=> console.log(`Server running on port: http://localhost:${PORT}`));
+app.listen(PORT, ()=> debug(`Server running on port: http://localhost:${chalk.green(PORT)}` ));
