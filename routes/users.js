93207c79857124b0d2f6623ac885b820b11c6538
xcixor <pndungu54@gmail.com>
2023-01-31 12:23:38 +0300

    refactor(auth): put authentication routes in a single file

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,15 @@
 const express = require('express');
 const userController = require('../controllers/users');
 const userValidate = require('../middleware/userValidation');
+const { checkNotAuthenticated } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.get('/', userController.getUsers);
+
+router.get('/register/', checkNotAuthenticated, (req, res) => {
+  res.render('users/register.ejs', { title: 'Registration' });
+});
 
 router.post('/register/', userValidate('createUser'), userController.createUser);
 
