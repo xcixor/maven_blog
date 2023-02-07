23056de54df0787f8894eb90a0d44512b7844180
xcixor <pndungu54@gmail.com>
2023-02-07 12:19:51 +0300

    feat(categories): display categories in index page

--- routes/blog.js
+++ routes/blog.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const passport = require('passport');
 const { checkNotAuthenticated, checkAuthenticated } = require('../middleware/auth');
-const { getPosts } = require('../services/postService');
-const { StatusCodes } = require('../utils/httpStatusCodes');
+const { getCategories } = require('../services/categoryService');
 
 const router = express.Router();
 
-router.get('', checkAuthenticated, (req, res) => {
-  let posts;
-  const getResponse = getPosts();
-  if (res.statusCode === StatusCodes.OK) {
-    posts = getResponse.response;
-  }
-  res.render('index', { title: 'Blog', user: req.user.email, posts });
+router.get('', checkAuthenticated, async (req, res) => {
+  const { categories } = await getCategories();
+  res.render('index', { title: 'Blog', categories });
 });
 
 router.get('/blog/login/', checkNotAuthenticated, (req, res) => {
