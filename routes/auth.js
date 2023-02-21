311302feb2787be1993219075ce5d322e3b50b1b
xcixor <pndungu54@gmail.com>
2023-02-21 12:20:07 +0300

    chore(auth): disable registration

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,10 @@ const { checkNotAuthenticated } = require('../middleware/auth');
 const { jwtLogin } = require('../controllers/auth');
 
 const router = express.Router();
-router.get('/login', checkNotAuthenticated, (req, res) => {
+router.get('/login/', checkNotAuthenticated, (req, res) => {
   res.render('auth/login.ejs', { title: 'Login' });
 });
 
-router.post('/login', jwtLogin);
+router.post('/login/', jwtLogin);
 
 module.exports = router;
